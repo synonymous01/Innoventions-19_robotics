09827966534514ad724de087828de4479bad5f10
synonymous01 <ibrahimsa01@hotmail.com>
2019-08-01 16:22:07 +0500

    modified readme file

--- round3_main/round3_code/header_main.h
+++ round3_main/round3_code/header_main.h
@@ -1,16 +1,21 @@
-const uint8_t rt_fwd = 15, lt_fwd = 16, lt_bkd = 18, rt_bkd = 19;
-const uint8_t trig = 12, echo = 13, rx = 0, tx = 1, send = 23, recv = 24;
-
 void setDir(int direction);
 void writeMotors(int state_rt_fwd, int state_lt_fwd, int state_rt_bkd, int state_lt_bkd);
 void bt_main();
 void line_main();
 void wall_main();
 
+
+// sensor (jumper) pins
 #define BT 20
-#define LINE 21
 #define WALL 22
 
+// sensor pins
+#define trig 12
+#define echo 13
+#define rx 0
+#define tx 1
+#define send 23
+#define recv 24
 
 #define FWD 0
 #define BKD 1
@@ -18,3 +23,9 @@ void wall_main();
 #define LFT_FWD 3
 #define RHT 4
 #define LFT 5
+
+//L293D pins
+#define rt_fwd 15
+#define lt_fwd 16
+#define lt_bkd 18
+#define rt_bkd 19
